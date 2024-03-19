0224dc01818bfed78d911725a129fabcffc241a1
thanhtai22 <tai127589@gmail.com>
2024-03-19 14:45:33 +0700

    res, login

--- client/src/containers/public/Navigation.js
+++ client/src/containers/public/Navigation.js
@@ -4,10 +4,11 @@ const Navigation = () => {
   return (
     <div className='w-screen flex justify-center items-center h-[40px] bg-secondary1 text-white'>
         <div className='w-1100'>
-            <Navigation/>
+            Navigation
         </div>
     </div>
   )
 }
 
 export default Navigation
+
---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+require('dotenv').config()
+import cors from 'cors'
+import initRoutes from './src/routes/index'
+import connectDB from "./src/config/connectDB";
+
+const app = express();
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["POST", "GET", "PUT", "DELETE"]
+}))
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extends: true }))
+
+initRoutes(app)
+connectDB()
+
+app.use('/', (req, res) => {
+    res.send('server is running')
+})
+
+const port = process.env.PORT || 8080
+const listener = app.listen(port, () => {
+    console.log(`server is running on port ${listener.address().port} `)
+})
